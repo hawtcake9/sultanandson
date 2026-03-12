35b2bf9b357de5d84a294802d65770050b7a06fd
hawtcake9 <stylerarshad85@gmail.com>
2026-03-12 13:09:03 +0500

    Update mediaProjects.ts

--- data/mediaProjects.ts
+++ data/mediaProjects.ts
@@ -10,10 +10,7 @@ export const MEDIA_PROJECTS: MediaProject[] = [
     slug: "solar-energy",
     title: "5KW Inverter Setup - Kala Khiel",
     description:
-      "Fri, 21 Nov 2025 - 12:00
-Solar Installations Showcase
-Located in Kala Khiel, this solar inverter installation project demonstrates our commitment to delivering efficient and reliable renewable energy solutions. 
-  The system is designed to maximize energy output while ensuring long-term performance. Explore the images to see the installation process, system setup, and the final outcome of our work",
+      "Fri, 21 Nov 2025 - 12:00 Solar Installations ShowcaseLocated in Kala Khiel, this solar inverter installation project demonstrates our commitment to delivering efficient and reliable renewable energy solutions. The system is designed to maximize energy output while ensuring long-term performance. Explore the images to see the installation process, system setup, and the final outcome of our work",
     images: [
       "/images/gallery1.jpeg",
       "/images/gallery2.jpeg",
